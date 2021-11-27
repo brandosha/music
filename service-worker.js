366fce1a4aa246cc23790cb26c9bc3417ab2f39b
brandosha <brandon.lantz@icloud.com>
2021-11-27 08:31:14 -0500

    Improve service worker

--- service-worker.js
+++ service-worker.js
@@ -1,39 +1,44 @@
 const cacheName = 'music-v1'
 
 self.addEventListener("fetch", event => {
-  const url = new URL(event.request.url)
-
   event.respondWith(
-    caches.match(event.request).then(response => {
-      if (url.origin !== location.origin) {
-        if (response) {
-          return response
-        } else {
-          return fetch(event.request).then(networkResponse => {
-            caches.open(cacheName).then(cache => {
-              cache.put(event.request, networkResponse.clone())
-            })
-
-            return networkResponse.clone()
-          })
-        }
-      } else {
-        return fetch(event.request).then(networkResponse => {
-          caches.open(cacheName).then(cache => {
-            cache.put(event.request, networkResponse.clone())
-          })
-  
-          return networkResponse.clone()
-        }).catch(error => {
-          console.error(error)
-  
-          if (response) {
-            return response
-          } else {
-            throw error
-          }
-        })
-      }
-    })
+    respond(event.request)
   )
 })
+
+async function respond(req) {
+  const url = new URL(req.url)
+
+  let cachedResponse = await caches.match(req)
+  if (url.hostname === 'localhost') {
+    try {
+      const networkResponse = await fetch(req)
+      cache(req, networkResponse.clone())
+
+      return networkResponse
+    } catch (err) {
+      if (cachedResponse) {
+        return cachedResponse
+      } else {
+        throw err
+      }
+    }
+  } else {
+    const networkFetch = fetch(req).then(networkResponse => {
+      cache(req, networkResponse.clone())
+
+      return networkResponse
+    })
+
+    if (cachedResponse) {
+      return cachedResponse
+    } else {
+      return networkFetch
+    }
+  }
+}
+
+async function cache(req, res) {
+  const cache = await caches.open(cacheName)
+  await cache.put(req, res)
+}
